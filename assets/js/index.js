37e269cd539b992cff661d9030520a7f7ee9b65c
Bachtiar Dwi <bachtiardwijulianto@gmail.com>
2023-10-24 07:31:59 +0700

    First Commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const typing = new Typed (".typing", {
+    strings: ['Fullstack Developer.', 'Freelancer.', 'Project Management.', 'Backend Developer.'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
